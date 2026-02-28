81c79d191640a5aaac16e953e81485217ec0b4c2
AmruthaBhamidipalli <lakshmiamrutha.0916@gmail.com>
2026-03-01 02:25:55 +0530

    updated schemaa

--- app/schemas.py
+++ app/schemas.py
@@ -1,20 +1,11 @@
-from pydantic import BaseModel, Field
-from typing import Dict
-
+from pydantic import BaseModel
 
 class BatchInput(BaseModel):
-    Drying_Temp: float = Field(..., gt=0)
-    Drying_Time: float = Field(..., gt=0)
+    Granulation_Time: float
+    Binder_Amount: float
+    Drying_Temp: float
+    Drying_Time: float
     Compression_Force: float
     Machine_Speed: float
-    Binder_Amount: float
-    Flow_Rate_LPM: float
-    Motor_Speed_RPM: float
-    Vibration_mm_s: float
-
-
-class PredictionResponse(BaseModel):
-    predictions: Dict[str, float]
-    prediction_intervals: Dict[str, Dict[str, float]]
-    inference_time_ms: float
-    model_version: str
+    Lubricant_Conc: float
+    Moisture_Content: float
